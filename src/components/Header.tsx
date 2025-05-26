470560e7e43367555430038c8d180c4464237428
ZH-170 <zihenglim0710@gmail.com>
2025-05-27 01:08:35 +0800

    Debug minor error

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -4,7 +4,7 @@ import Link from "next/link";
 function Header() {
   return (
     <header className="bg-white px-10 py-5 sm:px-20 md:px-40 flex flex-row">
-      <Link href="/" className="flex-1 text-2xl">Ryan's Corner</Link>
+      <Link href="/" className="flex-1 text-2xl">Ryan&apos;s Corner</Link>
       <nav className="flex-5">
         <ul className="flex flex-row-reverse gap-3 sm:gap-5 md:gap-8 lg:gap-10">
           <li>
