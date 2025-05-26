e9acd5bc10a62f31a69db3c53b29685f7d96b0b5
ZH-170 <zihenglim0710@gmail.com>
2025-05-26 16:41:31 +0800

    Update Header.tsx

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,13 +1,14 @@
 import React from 'react';
+import Link from 'next/link'
 
 function Header() {
     return (
         <header className="bg-white py-5 px-10 sm:px-20 md:px-40">
             <nav>
                 <ul className="flex flex-row-reverse gap-3 sm:gap-5 lg:gap-10 md:gap-8">
-                    <li><a href="/">Home</a></li>
-                    <li><a href="/about">About</a></li>
-                    <li><a href="/blog">Blog</a></li>
+                    <li><Link href="/">Home</Link></li>
+                    <li><Link href="/about">About</Link></li>
+                    <li><Link href="/blog">Blog</Link></li>
                 </ul>
             </nav>
         </header>
