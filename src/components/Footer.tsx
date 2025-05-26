49816fe1b793bdf5db118b3310853d6a116e161a
ZH-170 <zihenglim0710@gmail.com>
2025-05-26 17:06:14 +0800

    Update Title and Description

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -1,11 +1,11 @@
-import React from 'react';
+import React from "react";
 
 function Footer() {
-    return(
-        <footer className="bg-white py-5 px-10 sm:px-20 md:px-40">
-            This is a footer~
-        </footer>
-    );
+  return (
+    <footer className="bg-white px-10 py-5 sm:px-20 md:px-40">
+      This is a footer~
+    </footer>
+  );
 }
 
 export default Footer;
--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,18 +1,24 @@
-import React from 'react';
-import Link from 'next/link'
+import React from "react";
+import Link from "next/link";
 
 function Header() {
-    return (
-        <header className="bg-white py-5 px-10 sm:px-20 md:px-40">
-            <nav>
-                <ul className="flex flex-row-reverse gap-3 sm:gap-5 lg:gap-10 md:gap-8">
-                    <li><Link href="/">Home</Link></li>
-                    <li><Link href="/about">About</Link></li>
-                    <li><Link href="/blog">Blog</Link></li>
-                </ul>
-            </nav>
-        </header>
-    );
+  return (
+    <header className="bg-white px-10 py-5 sm:px-20 md:px-40">
+      <nav>
+        <ul className="flex flex-row-reverse gap-3 sm:gap-5 md:gap-8 lg:gap-10">
+          <li>
+            <Link href="/">Home</Link>
+          </li>
+          <li>
+            <Link href="/about">About</Link>
+          </li>
+          <li>
+            <Link href="/blog">Blog</Link>
+          </li>
+        </ul>
+      </nav>
+    </header>
+  );
 }
 
 export default Header;
