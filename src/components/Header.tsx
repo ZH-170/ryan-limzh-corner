c96e757d15215074f9f3e71a170b3948f3ca4e55
ZH-170 <zihenglim0710@gmail.com>
2025-05-27 10:23:29 +0800

    Update minor changes to the Header

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -4,7 +4,7 @@ import Link from "next/link";
 function Header() {
   return (
     <header className="bg-white px-10 py-2 sm:px-20 md:px-40 lg:px-60 xl:px-80 flex flex-row shadow-2xs shadow-black">
-      <Link href="/" className="flex-1 text-md sm:text-2xl p-3">Ryan&apos;s Corner</Link>
+      <Link href="/" className="flex-2 text-md sm:text-2xl p-3">Ryan&apos;s Corner</Link>
       <nav className="py-3 px-5 flex-5">
         <ul className="flex flex-row-reverse gap-3 sm:gap-5 md:gap-8 lg:gap-10">
           
