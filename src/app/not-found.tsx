be0267b87eee13868540d363861b3718bc4bf723
ZH-170 <zihenglim0710@gmail.com>
2025-05-30 22:37:38 +0800

    Update not-found.tsx

--- src/app/not-found.tsx
+++ src/app/not-found.tsx
@@ -1,8 +1,17 @@
 export default function NotFound() {
   return (
-    <div className="flex h-dvh w-full flex-col items-center justify-center bg-white">
-      <h1 className="text-2xl">Error 404 - Page not found</h1>
+    <div className="flex h-dvh w-full flex-col items-center bg-white">
+     <pre style={{ fontFamily: 'monospace', whiteSpace: 'pre' }}>{`
+   _____  _______      _____   ___________
+  /  |  | \\   _  \\    /  |  |  \\_   _____/_____________  ___________ 
+ /   |  |_/  /_\\  \\  /   |  |_  |    __)_\\_  __ \\_  __ \\/  _ \\_  __ \\
+/    ^   /\\  \\_/   \\/    ^   /  |        \\|  | \\/|  | \\(  <_> )  | \\/
+\\____   |  \\_____  /\\____   |  /_______  /|__|   |__|   \\____/|__|   
+     |__|        \\/      |__|          \\/
+     `}</pre>
+           <h1 className="text-2xl">Error 404 - Page not found</h1>
       <p>We couldn&apos;t find this page.</p>
+      <br/>
     </div>
   );
 }
