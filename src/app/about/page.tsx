b0219faac7bed512df891090f6fd127fdd588db4
ZH-170 <zihenglim0710@gmail.com>
2025-05-30 17:59:22 +0800

    Added AboutMe page

--- src/app/about/page.tsx
+++ src/app/about/page.tsx
@@ -1,5 +1,27 @@
 import Link from "next/link";
 
 export default function About() {
-  return <Link href="/about/contact">Hello! I am Ryan!</Link>;
+  return (
+  <div className="bg-white rounded-xl mx-10 flex flex-col items-center">
+    <p className="sm:text-xl">Hi there! The name is Lim Zi Heng, and you can just call me Ryan. Nice to meet you!</p>
+    <br />
+
+    <div className="size-3/5 lg:size-2/5">
+    <p>I am a guy who have a lot of time but will make myself busy so that I don't have a lot of time. Maybe you might think that I like to be busy, but the truth is DEFINITELY NOT!</p>
+    <br />
+    <p>And maybe this is the reason for a "multitalented" me.</p>
+    <br />
+    <p>So, to not waste time, here are some tags about the fields I am currently playing in, and I have been struggling and improving in some subfields of those specific field.</p>
+    <p>#computerscience-cp #art-watercolor #music-piano&erhu #naturalscience</p>
+    <br />
+    <p> If you really want to know more about me (or you don't believe in any single one of the tags), feel free to check my social media accounts below.</p>
+    <br />
+    <p className="font-bold"> Have a nice day everyone!</p>
+    <br />
+    <p>P.S. Hopefully I can get this website out in a couple of days because there are a lot more ideas in my head and I really want to try them out after that!</p>
+
+    </div>
+  </div>
+    
+  );
 }
